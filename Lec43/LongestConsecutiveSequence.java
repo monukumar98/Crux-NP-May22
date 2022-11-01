9c5f8d0bbc0b2630d0cd4487d04a568c77f900d5
monukumar98 <monu012as@gmail.com>
2022-11-01 14:03:27 +0530

    Map

--- Lec43/LongestConsecutiveSequence.java
+++ Lec43/LongestConsecutiveSequence.java
@@ -1,10 +1,40 @@
 package Lec43;
 
+import java.util.HashMap;
+
 public class LongestConsecutiveSequence {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] arr = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
 
+	}
+
+	public static int LongestConsecutive(int[] arr) {
+		HashMap<Integer, Boolean> map = new HashMap<>();
+		for (int i = 0; i < arr.length; i++) {
+			if (map.containsKey(arr[i] - 1)) {
+				map.put(arr[i], false);
+			} else {
+				map.put(arr[i], true);
+			}
+
+			if (map.containsKey(arr[i] + 1)) {
+				map.put(arr[i] + 1, false);
+			}
+		}
+		int ans = 0;
+		for (Integer key : map.keySet()) {
+			if (map.get(key)) {
+				int c = 0;
+				while (map.containsKey(key)) {
+					key++;
+					c++;
+				}
+				ans = Math.max(ans, c);
+			}
+		}
+		return ans;
 	}
 
 }
